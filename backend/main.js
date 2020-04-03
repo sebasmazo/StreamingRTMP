4fad273f7e0a82241a32187be9218ed9d2b92d5b
sertibas <62997479+sertibas@users.noreply.github.com>
2020-04-03 10:08:41 -0500

    Add files via upload

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,11 @@
-navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream) => {
-    console.log(stream)
+'use strict';
+navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream) => { //Pide permiso al usuario para acceder al hardware
+    console.log(stream) //Indica por consola el inicio del stream
 
-    let video = document.getElementById('video')
+    let video = document.getElementById('video') //Regresa una referencia para el video
 
-    video.srcObject = stream
-    video.onloadedmetadata = (ev => video.play())
+    video.srcObject = stream //Streaming
+    video.onloadedmetadata = (ev => video.play()) //Inicia la transmisión del video cuando las condiciones se hayan especificado
     
 
-}).catch((err)=>console.log(err))
+}).catch((err)=>console.log(err)) // Control de errores
